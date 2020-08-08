2bd3908ba33b3ed5570ac812799844b11c735f3e
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-08-08 18:57:40 +0800

    trying to fix router issue

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const history = require('connect-history-api-fallback');
 
 app = express();
 
+const staticFileMiddleware = serveStatic(path.join(__dirname + '/dist'));
+app.use(staticFileMiddleware);
 app.use(history({
     disableDotRule: true,
     verbose: true
 }));
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 
 const port = process.env.PORT || 80;
 app.listen(port);
