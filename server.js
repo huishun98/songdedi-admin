03a310c89e3398a066055b71747b9e40d64e348e
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-08-08 18:50:39 +0800

    added history fallback

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const history = require('connect-history-api-fallback');
 
 app = express();
+
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
